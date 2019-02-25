935b33e76d5b7a2eab7fbf17bfb7665354821cee
Julian Ros <julianrosbcn@outlook.com>
2019-02-25 18:38:37 +0100

    change in consoleapp importer proj settings,path chanegs

---
+++ CryptoMiddleware/CryptoImporter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CryptoImporter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KrakenData.RemoveQuoteData();
+            KrakenData.RemoveIndicatorsData();
+            while (true)
+            { 
+                KrakenFunctions.GetKrakenMarketData();
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
