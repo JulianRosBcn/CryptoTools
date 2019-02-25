fa37112c4843448086098caa7cecabd1559e3c55
Julian Ros <julianrosbcn@outlook.com>
2019-02-25 12:03:53 +0100

    Fixed SP SMACalcuation

--- StoredProcedures/indicators/SMACalculation.sql
+++ StoredProcedures/indicators/SMACalculation.sql
@@ -6,10 +6,15 @@ BEGIN
 
 	SET time_zone='+01:00';
 	
-	INSERT INTO kraken.indicators (sma5min) SELECT AVG(last) FROM kraken.quotes WHERE timestamp < (NOW() - INTERVAL 5 minute);
-	INSERT INTO kraken.indicators (sma20min) SELECT AVG(last) FROM kraken.quotes WHERE timestamp < (NOW() - INTERVAL 20 minute);
-	INSERT INTO kraken.indicators (sma60min) SELECT AVG(last) FROM kraken.quotes WHERE timestamp < (NOW() - INTERVAL 60 minute);
-	INSERT INTO kraken.indicators (sma24h) SELECT AVG(last) FROM kraken.quotes WHERE timestamp < (NOW() - INTERVAL 24 hour);
-	INSERT INTO kraken.indicators (timestamp) VALUES (NOW());
+	SET @_sma5min := (SELECT AVG(last) FROM kraken.quotes WHERE timestamp > (NOW() - INTERVAL 5 minute));
+	SET @_sma20min := (SELECT AVG(last) FROM kraken.quotes WHERE timestamp > (NOW() - INTERVAL 20 minute));
+	SET @_sma60min := (SELECT AVG(last) FROM kraken.quotes WHERE timestamp > (NOW() - INTERVAL 60 minute));
+	SET @_sma24h := (SELECT AVG(last) FROM kraken.quotes WHERE timestamp > (NOW() - INTERVAL 24 hour));
+	SET @_timestamp := (NOW());
+	
+	
+	
+	INSERT INTO kraken.indicators (`sma5min`,`sma20min`,`sma60min`,`sma24h`,`timestamp`)
+    VALUES (@_sma5min,@_sma20min,@_sma60min,@_sma24h,@_timestamp);
 	
 END
