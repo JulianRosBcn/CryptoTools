2749eb90ed82e830df0224299e1e80cc0123cacc
Julian Ros <julianrosbcn@outlook.com>
2019-02-27 16:26:10 +0100

    Added new SPs for volumeFlow indicators and alamrs. Reorganization

--- StoredProcedures/indicators/IndicatorsSchedule.sql
+++ StoredProcedures/indicators/IndicatorsSchedule.sql
@@ -1,9 +1,23 @@
 SET GLOBAL event_scheduler = ON
 
-CREATE EVENT UpdateIndicators
+CREATE DEFINER=`root`@`%` PROCEDURE `UpdateIndicators`()
+BEGIN
+	CALL SMACalculation();
+	CALL VolumeFlowCalculation();
+END
+
+CREATE DEFINER=`root`@`%` PROCEDURE `UpdateAlarms`()
+BEGIN
+	CALL SMATrending();
+	CALL VolumeFlowTrending();
+END
+
+CREATE EVENT Event_Indicators
     ON SCHEDULE EVERY 1 MINUTE
-    DO CALL SMACalculation();
-	
-CREATE EVENT UpdateAlarms
+    DO 
+		CALL UpdateIndicators();
+		
+CREATE EVENT Event_Alarms
     ON SCHEDULE EVERY 1 MINUTE
-    DO CALL SMATrending();
+    DO 
+		CALL UpdateAlarms();
