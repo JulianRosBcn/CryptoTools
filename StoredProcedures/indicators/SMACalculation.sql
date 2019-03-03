b35d0b151f5b7699d91cf2ea27c284fc8ed51537
Julian Ros <julianrosbcn@outlook.com>
2019-03-03 17:39:42 +0100

    Added support for multicurrency to indicators SPs

--- StoredProcedures/indicators/SMACalculation.sql
+++ StoredProcedures/indicators/SMACalculation.sql
@@ -7,22 +7,22 @@ BEGIN
 
 	SET time_zone='+01:00';
 	
-	SET @query = CONCAT('SELECT AVG(last) FROM markets.',_market,'_quotes WHERE (timestamp > (NOW() - INTERVAL 5 minute))  AND (coinpair = "' + _coinpair + '") INTO @query_output');
+	SET @query = CONCAT('SELECT AVG(last) FROM markets.',_market,'_quotes WHERE (timestamp > (NOW() - INTERVAL 5 minute))  AND (coinpair = "',_coinpair, '") INTO @query_output');
 	PREPARE exec_query FROM @query;
 	EXECUTE exec_query;
 	SET @_sma5min := @query_output;
 	
-	SET @query = CONCAT('SELECT AVG(last) FROM markets.',_market,'_quotes WHERE timestamp > (NOW() - INTERVAL 20 minute) AND coinpair = "' + _coinpair + '") INTO @query_output');
+	SET @query =CONCAT('SELECT AVG(last) FROM markets.',_market,'_quotes WHERE (timestamp > (NOW() - INTERVAL 20 minute))  AND (coinpair = "',_coinpair, '") INTO @query_output');
 	PREPARE exec_query FROM @query;
 	EXECUTE exec_query;
 	SET @_sma20min := @query_output;
 	
-	SET @query = CONCAT('SELECT AVG(last) FROM markets.',_market,'_quotes WHERE timestamp > (NOW() - INTERVAL 60 minute) AND coinpair = "' + _coinpair + '") INTO @query_output');
+	SET @query = CONCAT('SELECT AVG(last) FROM markets.',_market,'_quotes WHERE (timestamp > (NOW() - INTERVAL 60 minute))  AND (coinpair = "',_coinpair, '") INTO @query_output');
 	PREPARE exec_query FROM @query;
 	EXECUTE exec_query;
 	SET @_sma60min := @query_output;
 	
-	SET @query = CONCAT('SELECT AVG(last) FROM markets.',_market,'_quotes WHERE timestamp > (NOW() - INTERVAL 24 hour) AND coinpair = "' + _coinpair +'") INTO @query_output');
+	SET @query = CONCAT('SELECT AVG(last) FROM markets.',_market,'_quotes WHERE (timestamp > (NOW() - INTERVAL 24 hour))  AND (coinpair = "',_coinpair, '") INTO @query_output');
 	PREPARE exec_query FROM @query;
 	EXECUTE exec_query;
 	SET @_sma24h := @query_output;
@@ -31,9 +31,9 @@ BEGIN
 	SET @_timestamp := (NOW());
 	
 	
-	IF (_market = 'kraken') THEN INSERT INTO markets.kraken_indicators (`coinpair`,`sma5min`,`sma20min`,`sma60min`,`sma24h`,`timestamp`) VALUES (_coinpair,@_sma5min,@_sma20min,@_sma60min,@_sma24h,@_timestamp);
+	IF (_market = 'kraken') THEN INSERT INTO analytics.kraken_indicators (`coinpair`,`sma5min`,`sma20min`,`sma60min`,`sma24h`,`timestamp`) VALUES (_coinpair,@_sma5min,@_sma20min,@_sma60min,@_sma24h,@_timestamp);
 	END IF;
-    IF (_market = 'binance') THEN INSERT INTO markets.binance_indicators (`coinpair`,`sma5min`,`sma20min`,`sma60min`,`sma24h`,`timestamp`) VALUES (_coinpair,@_sma5min,@_sma20min,@_sma60min,@_sma24h,@_timestamp);
+    IF (_market = 'binance') THEN INSERT INTO analytics.binance_indicators (`coinpair`,`sma5min`,`sma20min`,`sma60min`,`sma24h`,`timestamp`) VALUES (_coinpair,@_sma5min,@_sma20min,@_sma60min,@_sma24h,@_timestamp);
 	END IF;
 	
 END
--- StoredProcedures/indicators/VolumenFlowCalculation.sql
+++ StoredProcedures/indicators/VolumenFlowCalculation.sql
@@ -7,19 +7,19 @@ BEGIN
 
 	SET time_zone='+01:00';
 	
-	SET @query = CONCAT('SELECT SUM(volume) FROM markets.',_market,'_quotes WHERE timestamp > (NOW() - INTERVAL 5 minute ) AND coinpair = ' + _coinpair + ' INTO @query_output');
+	SET @query = CONCAT('SELECT SUM(volume) FROM markets.',_market,'_quotes WHERE (timestamp > (NOW() - INTERVAL 5 minute))  AND (coinpair = "',_coinpair, '") INTO @query_output');
 	PREPARE exec_query FROM @query;
 	EXECUTE exec_query;
 	SET @_volume5min := @query_output;
 	
-	SET @query = CONCAT('SELECT SUM(volume) FROM markets.',_market,'_quotes WHERE timestamp > (now() - interval 65 minute) AND timestamp < (now() - interval 5 minute) AND coinpair = ' + _coinpair + ' INTO @query_output');
+	SET @query = CONCAT('SELECT SUM(volume) FROM markets.',_market,'_quotes WHERE (timestamp > (now() - interval 65 minute)) AND (timestamp < (now() - interval 5 minute))  AND (coinpair = "',_coinpair, '") INTO @query_output');
 	PREPARE exec_query FROM @query;
 	EXECUTE exec_query;
 	SET @_volume60min := @query_output;
 	
 	
 	IF (_market = 'kraken') THEN
-		UPDATE markets.kraken_indicators
+		UPDATE analytics.kraken_indicators
 		SET `volume5min` = @_volume5min, 
 			`volume60min` = @_volume60min
 		WHERE `coinpair` = _coinpair
@@ -27,7 +27,7 @@ BEGIN
 	END IF;
 	
 	IF (_market = 'binance') THEN
-		UPDATE markets.binance_indicators
+		UPDATE analytics.binance_indicators
 		SET `volume5min` = @_volume5min, 
 			`volume60min` = @_volume60min
 		WHERE `coinpair` = _coinpair
