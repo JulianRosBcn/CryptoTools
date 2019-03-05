4e5165ab3cd56b48323aeb6641af7677e1f54e00
Julian Ros <julianrosbcn@outlook.com>
2019-03-05 23:31:20 +0100

    Added OrderManager 4eyes principle. Under testing

--- StoredProcedures/orders/OrderManager.sql
+++ StoredProcedures/orders/OrderManager.sql
@@ -1,7 +1,6 @@
 CREATE DEFINER=`root`@`%` PROCEDURE `OrderManager`(
 _coinpair VARCHAR(20)
 )
-
 BEGIN
 
 	-- Variables to store order and order type when a position is opened/closed in the market
@@ -14,13 +13,13 @@ BEGIN
 
 	
 	-- Kraken gathering
-	SET @query = CONCAT('SELECT `order` FROM analytics.kraken_signals WHERE (timestamp > (NOW() - INTERVAL 5 minute))  AND (coinpair = "',_coinpair, '") INTO @query_output');
+	SET @query = CONCAT('SELECT `order` FROM analytics.kraken_signals WHERE (timestamp > (NOW() - INTERVAL 5 minute))  AND (coinpair = "',_coinpair, '") ORDER BY timestamp DESC LIMIT 1 INTO @query_output');
 	PREPARE exec_query FROM @query;
 	EXECUTE exec_query;
 	SET _krakenstate := @query_output;
 	
 		-- Binance gathering
-	SET @query = CONCAT('SELECT `order` FROM analytics.binance_signals WHERE (timestamp > (NOW() - INTERVAL 5 minute))  AND (coinpair = "',_coinpair, '") INTO @query_output');
+	SET @query = CONCAT('SELECT `order` FROM analytics.binance_signals WHERE (timestamp > (NOW() - INTERVAL 5 minute))  AND (coinpair = "',_coinpair, '") ORDER BY timestamp DESC LIMIT 1 INTO @query_output');
 	PREPARE exec_query FROM @query;
 	EXECUTE exec_query;
 	SET _binancestate := @query_output;
@@ -28,6 +27,9 @@ BEGIN
 	
 	
 	SET _lastorder := (SELECT `order` FROM orders  WHERE (coinpair = _coinpair ) ORDER BY timestamp DESC LIMIT 1);
+	
+	IF (_lastorder IS NULL) THEN SET _lastorder="none";
+	END IF;
 	
 	-- buy
 	IF  (_binancestate = "buy") AND (_krakenstate = "buy") AND (_lastorder <> "buy") THEN 
@@ -48,4 +50,3 @@ BEGIN
 
 	-- SELECT _krakenstate, _binancestate, _order, _lastorder;
 END
-	
